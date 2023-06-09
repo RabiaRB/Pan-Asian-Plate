18cf0fbfa688df9646505b3ffa8c5876d2b5c410
Rabiya Butt <air.bay.x@gmail.com>
2023-06-09 19:32:41 +0500

    added responsiveness and page elements

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 
+const form = document.querySelector('.inputForm');
+form.addEventListener('submit', function(event) {
+   event.preventDefault();
+   const emailInput = document.querySelector('.emailInput');
+   const email = emailInput.value.trim();
+
+   if (email === '') {
+      alert('Please enter your email.');
+      emailInput.focus();
+   } else if (!isValidEmail(email)) {
+      alert('Please enter a valid email address.');
+      emailInput.focus();
+   } else {
+      alert('Thank you for subscribing with email: ' + email);
+      form.reset();
+   }
+});
+
+function isValidEmail(email) {
+   // Regular expression pattern for validating email addresses
+   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   return emailPattern.test(email);
+}
